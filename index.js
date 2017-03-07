85b1fe42a8817e18095c47cb342e02604c2c4841
Robert Nagy <ronag89@gmail.com>
2017-03-07 15:30:15 +0100

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var randomBytes = require('randombytes')
 
 var ALPHABET = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ~abcdefghijklmnopqrstuvwxyz_'
 
-module.exports.encode = function (number) {
+function encode (number) {
   var str = ''
 
   for (var n = 0; number > 0; ++n) {
@@ -13,19 +13,19 @@ module.exports.encode = function (number) {
   return str
 }
 
-module.exports.decode = function (str) {
+function decode (str) {
   var number = 0
 
   for (var n = 0; n < str.length; ++n) {
-    number = ALPHABET.indexOf(str[n]) + number * 64
+    number = ALPHABET.indexOf(str[n]) + (number * 64)
   }
 
   return number
 }
 
-module.exports.xuid = function nuid () {
-  var str = module.exports.encode(Date.now()) +
-            module.exports.encode(parseInt(randomBytes(6).toString('hex'), 16))
+function xuid () {
+  var str = encode(Date.now()) +
+            encode(parseInt(randomBytes(6).toString('hex'), 16))
 
   while (str.length < 14) {
     str = str + '0'
@@ -34,8 +34,9 @@ module.exports.xuid = function nuid () {
   return str.substr(0, 14)
 }
 
-module.exports.date = function (id) {
-  return new Date(module.exports.decode(id.slice(0, 7)))
+xuid.create = xuid
+xuid.date = function (id) {
+  return new Date(decode(id.slice(0, 7)))
 }
 
-module.exports.default = module.exports.xuid
+module.exports = xuid
