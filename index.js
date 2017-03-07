b3dd1826b42f11b8bdb630c09a726764947baf9c
Robert Nagy <ronag89@gmail.com>
2017-03-07 11:29:31 +0100

    feat(date): Added date function to extract date from id.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var randomBytes = require('randombytes')
 
 var ALPHABET = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ~abcdefghijklmnopqrstuvwxyz_'
 
-function encode (number) {
+module.exports.encode = function (number) {
   var str = ''
 
   for (var n = 0; number > 0; ++n) {
@@ -13,9 +13,19 @@ function encode (number) {
   return str
 }
 
-module.exports.default = function nuid () {
-  var str = encode(Date.now()) +
-            encode(parseInt(randomBytes(6).toString('hex'), 16))
+module.exports.decode = function (str) {
+  var number = 0
+
+  for (var n = 0; n < str.length; ++n) {
+    number = ALPHABET.indexOf(str[n]) + number * 64
+  }
+
+  return number
+}
+
+module.exports.xuid = function nuid () {
+  var str = module.exports.encode(Date.now()) +
+            module.exports.encode(parseInt(randomBytes(6).toString('hex'), 16))
 
   while (str.length < 14) {
     str = str + '0'
@@ -23,3 +33,9 @@ module.exports.default = function nuid () {
 
   return str.substr(0, 14)
 }
+
+module.exports.date = function (id) {
+  return new Date(module.exports.decode(id.slice(0, 7)))
+}
+
+module.exports.default = module.exports.xuid
