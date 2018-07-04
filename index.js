1fff75ea4c5199c14b66865ed1a925e2642dfa00
Martin Grönlund <martingronlund@live.se>
2018-07-04 12:08:47 +0200

    cleanup

--- index.js
+++ index.js
@@ -7,26 +7,22 @@ var time = Date.now()
 
 function encode (number) {
   var str = ''
-
   for (var n = 0; number > 0; ++n) {
     str = ALPHABET[Math.floor(number % ALPHABET.length)] + str
-    number = Math.floor(Math.floor(number / ALPHABET.length))
+    number = Math.floor(number / ALPHABET.length)
   }
-
   return str
 }
 
 function decode (str) {
   var number = 0
-
   for (var n = 0; n < str.length; ++n) {
     var i = ALPHABET.indexOf(str[n])
     if (i === -1) {
-      return undefined
+      return
     }
-    number = i + (number * ALPHABET.length)
+    number = i + number * ALPHABET.length
   }
-
   return number
 }
 
@@ -35,7 +31,7 @@ function xuid () {
   var now = Date.now()
   var date = encode(now).slice(-7)
   var random = ('000' + encode(counter)).slice(-3) +
-               encode(parseInt(randomBytes(6).toString('hex'), 16)).slice(-4)
+    encode(parseInt(randomBytes(6).toString('hex'), 16)).slice(-4)
 
   if (arguments.length > 0) {
     date = encode(arguments[0]).slice(-7)
@@ -57,15 +53,16 @@ function xuid () {
 }
 
 xuid.create = xuid
+
 xuid.date = function (id) {
   var number = id && decode(id.slice(0, 7))
   if (!number) {
-    return undefined
+    return
   }
   var date = new Date(number)
   var now = new Date(Date.now() + 86400000)
   if (date > now || date.getFullYear() < 2015) {
-    return undefined
+    return
   }
   return date
 }
